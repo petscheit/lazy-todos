44661824976f4f38ee8b06a7a03ca03d970ebb89
PaulEtscheit <paul@danielyan.de>
2016-11-14 16:30:05 +0100

    description for grounddb implementation

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,10 +1,10 @@
 import { Mongo } from 'meteor/mongo';
 
+//Uses GroundDB online on cordova build -> when app is launched offline version of db is loaded instantly -> no wait times till server catches up
 if(Meteor.isCordova){
    tasksMongo = new Mongo.Collection('tasks');
    tasksList = new Ground.Collection(tasksMongo);
 } else {
    tasksList = new Mongo.Collection('tasks');
 }
-
 export const Tasks = tasksList;
