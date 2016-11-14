940f10da810113e4428bad29d60bcb84b6e4ec03
PaulEtscheit <paul@danielyan.de>
2016-11-14 14:41:29 +0100

    remove accessRules

--- server/main.js
+++ server/main.js
@@ -2,5 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/api/tasks.js';
 
 Meteor.startup(() => {
-  App.accessRule('https://lazy-todo.herokuapp.com/*');
+  // App.accessRule('https://lazy-todo.herokuapp.com/*');
 });
