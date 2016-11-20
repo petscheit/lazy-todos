9793a5e97f26732d6247bac7880c704f3d4f84d1
PaulEtscheit <paul@danielyan.de>
2016-11-20 23:56:10 +0100

    added redo functionality, tasks can now be moved from completed back to open

--- imports/ui/completedTask.js
+++ imports/ui/completedTask.js
@@ -1,1 +1,17 @@
+import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';
+
 import './completedTask.html';
+
+Template.completedTask.events({
+  'click .redo'() {
+    Tasks.update(this._id, {
+      $set: { completed: false,
+              redoAt: new Date(),
+            },
+    });
+  },
+})
+
+
